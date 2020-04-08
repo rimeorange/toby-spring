ddfa8cf12f906a6facdfef3cb7ff758925dcfab2
rimeorange <rimeorange83@gmail.com>
2020-04-09 01:20:47 +0900

    Chapter_01-3 - 상속을 통한 확장 : Factory Method Pattern(서브클래스에서 구체적인 오브젝트 생성 방법을 결정하게 하는것) 적용

---
+++ chapter_01-3/src/main/java/springbook/user/dao/NUserDao.java
@@ -0,0 +1,13 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class NUserDao extends UserDao{
+
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        //N사 DB Connection 생성코드
+        return null;
+    }
+}
---
+++ chapter_01-3/src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,54 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class UserDao {
+
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values (?,?,?)"
+        );
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    //구현 코드는 제거되고 추상 메소드로 바뀌었다. 메소드의 구현은 서브클래스가 담당한다.
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+}
