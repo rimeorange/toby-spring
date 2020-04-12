f568121ca77a368e8c34a025feaba60be6e51b4c
rimeorange <rimeorange83@gmail.com>
2020-04-12 21:11:53 +0900

    Chapter_01-5 DaoFactory를 사용하는 애플리케이션 컨텍스트

---
+++ chapter_01-5/src/main/java/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,22 @@
+package springbook.user.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
+@Configuration
+public class DaoFactory {
+
+    //오브젝트 생성을 담당하는 IoC용 메소드라는 표
+    @Bean
+    public UserDao userDao() {
+        UserDao userDao = new UserDao(connectionMaker());
+        return userDao;
+    }
+
+    //분리해서 중복을 제거한 ConnectionMaker타입 오브젝트 생성 코드
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+}
