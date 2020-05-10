651966e7960d4e3fe2b25ac9820596d8921d9715
rimeorange <rimeorange83@gmail.com>
2020-05-10 17:56:49 +0900

    Chapter_02-2 UserDao - deleteAll(), getCount() add

---
+++ chapter_02-2/src/test/java/UserDaoTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserDaoTest {
+    @Test
+    public void addAndGet() throws SQLException{
+        ApplicationContext context =
+                new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        User user = new User();
+        user.setId("krazy");
+        user.setName("심원보");
+        user.setPassword("spring01");
+
+        dao.add(user);
+        assertThat(dao.getCount(), is(1));
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+
+    }
+}
