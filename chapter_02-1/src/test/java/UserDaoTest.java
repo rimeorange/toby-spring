f783f587098e986b85c241d519c37244100b063a
rimeorange <rimeorange83@gmail.com>
2020-05-10 17:51:27 +0900

    Chapter_02-1 Junit 단위테스트 추가

---
+++ chapter_02-1/src/test/java/UserDaoTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    @Test
+    public void addAndGet() throws SQLException{
+        ApplicationContext context =
+                new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("krazy");
+        user.setName("심원보");
+        user.setPassword("spring01");
+
+        dao.add(user);
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+
+    }
+}
