b27190c0f48b26c92f3a78ae5eab2a4a2f484b02
rimeorange <rimeorange83@gmail.com>
2020-05-10 18:20:46 +0900

    Chapter_2-3 getCount Test Add

---
+++ chapter_02-3/src/test/java/UserDaoTest.java
@@ -0,0 +1,59 @@
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserDaoTest {
+    @Test
+    public void addAndGet() throws SQLException{
+        ApplicationContext context =
+                new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        User user = new User();
+        user.setId("krazy");
+        user.setName("심원보");
+        user.setPassword("spring01");
+
+        dao.add(user);
+        assertThat(dao.getCount(), is(1));
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+
+    }
+
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("rime", "심원보", "spring01");
+        User user2 = new User("rime2", "심원보2", "spring02");
+        User user3 = new User("rime3", "심원보3", "spring03");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
+}
